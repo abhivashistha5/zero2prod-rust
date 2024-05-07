f8bdb86e579733fcc5501306a0c344269ea44fd7
Abhijeet Vashistha <abhivashistha5@gmail.com>
2024-05-07 22:27:32 +0530

    minor changes in migration script

--- migrations/20240507161720_create_subscription_table.sql
+++ migrations/20240507161720_create_subscription_table.sql
@@ -1,10 +1,10 @@
 -- migrations/20240507161720_create_subscription_table.sql
 
--- Create Subscription Table
+-- Create the subscriptions table
 CREATE TABLE subscriptions(
   id uuid NOT NULL,
   PRIMARY KEY (id),
   email TEXT NOT NULL UNIQUE,
   name TEXT NOT NULL,
-  subscribed_at timestamp NOT NULL
+  subscribed_at timestamptz NOT NULL
 );
--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -34,7 +34,8 @@ DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
 DB_NAME="${POSTGRES_DB:=newsletter}"
 DB_PORT="${POSTGRES_PORT:=5432}"
 DB_HOST="${POSTGRES_HOST:=localhost}"
-
+export PGPASSWORD="${DB_PASSWORD}"
+export DATABASE_URL="postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}"
 
 # Launch docker
 if [[ -z "SKIP_DOCKER" ]]; then
@@ -48,8 +49,6 @@ if [[ -z "SKIP_DOCKER" ]]; then
 fi
 
 # Check for postgres ready
-export PGPASSWORD="${DB_PASSWORD}"
-export DATABASE_URL="postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}"
 until ${POSTGRES_CLI} -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -l &> /dev/null; do
   >&2 echo "Postgres is still unavailable - sleeping"
   sleep 1
