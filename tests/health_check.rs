6f7853bac0701a87b97f9ef0ead02f86d06041f3
Abhijeet Vashistha <abhivashistha5@gmail.com>
2024-05-08 18:55:20 +0530

    added database configuration

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,8 @@
 use std::net::TcpListener;
 
+use sqlx::{Connection, PgConnection};
+use zero2prod_rust::configuration;
+
 #[allow(clippy::let_underscore_future)]
 async fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
@@ -31,8 +34,13 @@ async fn health_check_works() {
 #[tokio::test]
 async fn subscribe_returns_200_valid_form_data() {
     let address = spawn_app().await;
-    let client = reqwest::Client::new();
+    let config = configuration::get_configuration().expect("Failed to load configuration");
+    let connection_string = config.database.connection_string();
+    let mut db = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to db");
 
+    let client = reqwest::Client::new();
     let body = "name=Bruce%20Wayne&email=bruce%40wayne.com";
 
     let response = client
@@ -44,6 +52,14 @@ async fn subscribe_returns_200_valid_form_data() {
         .expect("Failed to execute request.");
 
     assert_eq!(reqwest::StatusCode::OK, response.status());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
+        .fetch_one(&mut db)
+        .await
+        .expect("Failed to fetch subscriptions");
+
+    assert_eq!(saved.email, "bruce@wayne.com");
+    assert_eq!(saved.name, "Bruce Wayne");
 }
 
 #[tokio::test]
