f580d8e759d121e9e0fde212729c65f8ebc382f4
Abhijeet Vashistha <abhivashistha5@gmail.com>
2024-05-09 00:28:20 +0530

    improved test case to use connection pool

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,10 +1,15 @@
 use std::net::TcpListener;
 
-use sqlx::{Connection, PgConnection, PgPool};
+use sqlx::PgPool;
 use zero2prod_rust::configuration;
 
+pub struct TestApp {
+    pub address: String,
+    pub db_pool: PgPool,
+}
+
 #[allow(clippy::let_underscore_future)]
-async fn spawn_app() -> String {
+async fn spawn_app() -> TestApp {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let config = configuration::get_configuration().expect("Failed to load configuration");
 
@@ -13,22 +18,25 @@ async fn spawn_app() -> String {
     let db_pool = PgPool::connect(&connection_string)
         .await
         .expect("Failed to connect to db");
-    let server = zero2prod_rust::startup::run(listener, db_pool)
+    let server = zero2prod_rust::startup::run(listener, db_pool.clone())
         .await
         .expect("Failed to bind address");
     let _ = tokio::spawn(server);
 
-    format!("http://127.0.0.1:{}", port)
+    TestApp {
+        address: format!("http://127.0.0.1:{}", port),
+        db_pool,
+    }
 }
 
 #[tokio::test]
 async fn health_check_works() {
-    let address = spawn_app().await;
+    let app = spawn_app().await;
 
     let client = reqwest::Client::new();
 
     let response = client
-        .get(format!("{}/ping", address))
+        .get(format!("{}/ping", app.address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -39,19 +47,13 @@ async fn health_check_works() {
 
 #[tokio::test]
 async fn subscribe_returns_200_valid_form_data() {
-    let address = spawn_app().await;
-
-    let config = configuration::get_configuration().expect("Failed to load configuration");
-    let connection_string = config.database.connection_string();
-    let mut db = PgConnection::connect(&connection_string)
-        .await
-        .expect("Failed to connect to db");
+    let app = spawn_app().await;
 
     let client = reqwest::Client::new();
     let body = "name=Bruce%20Wayne&email=bruce%40wayne.com";
 
     let response = client
-        .post(format!("{}/subscriptions", address))
+        .post(format!("{}/subscriptions", app.address))
         .header("Content-Type", "application/x-www-form-urlencoded")
         .body(body)
         .send()
@@ -61,7 +63,7 @@ async fn subscribe_returns_200_valid_form_data() {
     assert_eq!(reqwest::StatusCode::OK, response.status());
 
     let saved = sqlx::query!("SELECT email, name FROM subscriptions")
-        .fetch_one(&mut db)
+        .fetch_one(&app.db_pool)
         .await
         .expect("Failed to fetch subscriptions");
 
@@ -71,7 +73,7 @@ async fn subscribe_returns_200_valid_form_data() {
 
 #[tokio::test]
 async fn subscribe_returns_400_invalid_request() {
-    let address = spawn_app().await;
+    let app = spawn_app().await;
     let client = reqwest::Client::new();
 
     let inputs = vec![
@@ -82,7 +84,7 @@ async fn subscribe_returns_400_invalid_request() {
 
     for (body, error_message) in inputs {
         let response = client
-            .post(format!("{}/subscriptions", address))
+            .post(format!("{}/subscriptions", app.address))
             .header("Content-Type", "application/x-www-form-urlencoded")
             .body(body)
             .send()
@@ -95,6 +97,5 @@ async fn subscribe_returns_400_invalid_request() {
             "Api did not failed with bad request error: {}",
             error_message
         );
-        // assert_eq!(error_message, response.text().await.unwrap().as_str());
     }
 }
