30ffefb7c209dd18b91caa77bbb7fd9b253c0cd0
Abhijeet Vashistha <abhivashistha5@gmail.com>
2024-05-11 00:57:57 +0530

    Added secrecy

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::net::TcpListener;
 
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use zero2prod_rust::{
     configuration,
@@ -15,7 +16,7 @@ async fn main() -> Result<(), std::io::Error> {
 
     let config = configuration::get_configuration().expect("Failed to load config");
     let address = format!("127.0.0.1:{}", config.application_port);
-    let db_connection_pool = PgPool::connect(&config.database.connection_string())
+    let db_connection_pool = PgPool::connect(config.database.connection_string().expose_secret())
         .await
         .expect("Failed to connect to database");
     let listener = TcpListener::bind(address).expect("Failed to bind to port");
