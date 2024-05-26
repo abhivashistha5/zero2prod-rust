4065250bdd777f81eb89d905ed5ddf39471d28cd
Abhijeet Vashistha <abhivashistha5@gmail.com>
2024-05-26 18:55:23 +0530

    refactored confirmation link test cases

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -14,6 +14,11 @@ pub struct TestApp {
     pub email_server: MockServer,
 }
 
+pub struct ConfirmationLinks {
+    pub html: reqwest::Url,
+    pub plain_text: reqwest::Url,
+}
+
 impl TestApp {
     pub async fn post_subscriptions(&self, body: String) -> reqwest::Response {
         let client = reqwest::Client::new();
@@ -24,6 +29,35 @@ impl TestApp {
             .send()
             .await
             .expect("Failed to execute request.")
+    }
+
+    pub fn get_confirmation_links(&self, email_request: &wiremock::Request) -> ConfirmationLinks {
+        let body: serde_json::Value = serde_json::from_slice(&email_request.body).unwrap();
+
+        let get_links = |s: &str| {
+            let links: Vec<linkify::Link> = linkify::LinkFinder::new()
+                .links(s)
+                .filter(|l| *l.kind() == linkify::LinkKind::Url)
+                .collect();
+
+            assert_eq!(links.len(), 1);
+            let raw_confirmation_link = links[0].as_str().to_owned();
+            let mut confirmation_link: reqwest::Url =
+                reqwest::Url::parse(&raw_confirmation_link).unwrap();
+
+            assert_eq!(confirmation_link.host_str().unwrap(), "localhost");
+
+            confirmation_link.set_port(Some(self.port)).unwrap();
+            confirmation_link
+        };
+
+        let html_link = get_links(body["HtmlBody"].as_str().unwrap());
+        let plain_text_link = get_links(body["TextBody"].as_str().unwrap());
+
+        ConfirmationLinks {
+            html: html_link,
+            plain_text: plain_text_link,
+        }
     }
 }
 
--- tests/api/subscriptions_confirm_tests.rs
+++ tests/api/subscriptions_confirm_tests.rs
@@ -1,4 +1,3 @@
-use reqwest::Url;
 use sqlx::PgPool;
 use wiremock::{
     matchers::{method, path},
@@ -33,26 +32,10 @@ async fn link_sent_in_mail_returns_200_when_called(db_pool: PgPool) {
     app.post_subscriptions(body.into()).await;
 
     let email_request = &app.email_server.received_requests().await.unwrap()[0];
-    let body: serde_json::Value = serde_json::from_slice(&email_request.body).unwrap();
 
-    let get_links = |s: &str| -> String {
-        let links: Vec<linkify::Link> = linkify::LinkFinder::new()
-            .links(s)
-            .filter(|l| *l.kind() == linkify::LinkKind::Url)
-            .collect();
+    let confirmation_links = app.get_confirmation_links(email_request);
 
-        assert_eq!(links.len(), 1);
-        links[0].as_str().to_owned()
-    };
-
-    let raw_confirmation_link = get_links(body["HtmlBody"].as_str().unwrap());
-    let mut confirmation_link: Url = Url::parse(&raw_confirmation_link).unwrap();
-
-    assert_eq!(confirmation_link.host_str().unwrap(), "localhost");
-
-    confirmation_link.set_port(Some(app.port)).unwrap();
-
-    let response = reqwest::get(confirmation_link).await.unwrap();
+    let response = reqwest::get(confirmation_links.html).await.unwrap();
 
     assert_eq!(response.status(), reqwest::StatusCode::OK);
 }
